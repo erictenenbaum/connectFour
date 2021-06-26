fd3ee9ae301b89286137688b53304dfb158c17f0
Eric Tenenbaum <erictenenbaum@Erics-MacBook-Pro.local>
2021-06-26 15:30:51 -0600

    pre-deployment tiddying up

--- src/utils/dynamo.ts
+++ src/utils/dynamo.ts
@@ -1,6 +1,9 @@
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 
-export const documentClient: DocumentClient = new DocumentClient({
-  region: "localhost",
-  endpoint: "http://localhost:8000",
-});
+export const documentClient: DocumentClient =
+  process.env.IS_OFFLINE === "true"
+    ? new DocumentClient({
+        region: "localhost",
+        endpoint: "http://localhost:8000",
+      })
+    : new DocumentClient();
